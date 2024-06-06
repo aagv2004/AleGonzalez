b48d3ab7cf85d4c745bdb4615837cdf2a82dab52
Alejandro González <andresg.vergara04@gmail.com>
2024-06-06 00:56:20 -0400

    commit desde casa 1
    Se logró:
    1.- accesibilidad, tamaño font y contraste.
    2.a Tener 8 campos mínimos diferentes.
    2.b 1 input radio (o checkbox)
    2.c 1 textarea
    2.d 1 select
    4.- correcciones.
    6.- documentación (comentarios)
    registro de datos.
    se trabaja en las validaciones (aún registra incluso si los datos son incorrectos)

--- assets/js/promesas.js
+++ assets/js/promesas.js
@@ -3,7 +3,7 @@ import { addDoc, collection } from "https://www.gstatic.com/firebasejs/10.12.2/f
 import { db } from "./firebase.js"
 
 // Promesa de registro de una persona al formulario de contacto
-export const registrarContacto = async(persona) => {
-    docRef = collection(db, "personas")
-    dato = await addDoc(docRef, persona)
+export const registrarContacto = async(contacto) => {
+    console.log(contacto)
+    const docRef = await addDoc(collection(db, "contactos"), contacto);
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,8 @@ import { registrarContacto } from "./promesas.js";
 window.addEventListener("load", ()=> {
     document.getElementById("btnContraste").addEventListener("click", contraste);
     document.getElementById("btnFuente").addEventListener("click", fuente);
-
-    document.getElementById("btnEnviar").addEventListener("click", registrar)
+    document.getElementById("btnEnviar").addEventListener("click", registrar);
 })
-
 
 // Función que permite registrar
 const registrar = () => {
@@ -14,11 +12,58 @@ const registrar = () => {
     let eApellido = document.getElementById("apellido");
     let eTelefono = document.getElementById("telefono");
     let eMotivo = document.getElementById("motivo");
-    let eCheckbox = document.getElementsByTagName("asuntoImportante");
+    let eInstagram = document.getElementById("instagram");
+    let ePaginaweb = document.getElementById("paginaweb");
+    let eFacebook = document.getElementById("facebook");
 
 
+    // Recupero los elementos con el nombre "radio" (type: NodelistOfHtmlElement) y lo transformamos en un array, iteramos el arreglo.
+    // Buscamos aquel elemento dentro del array en el cual ".checked === true" y lo guardamos en "radioElegido" esto hará que "elemento.value"
+    // Muestre el valor de aquel elemento que está chequeado. En caso contrario mostrará en consola que el elemento no está chequeado y seguirá buscando.
+    // (Se declararon como constantes principalmente porque los tipos de variable "let" y "var" en un comienzo no hacía lo esperado)
+    let eRadio = document.getElementsByName("radio");
+    let eRadioArreglo = Array.from(eRadio)
+    for (let index = 0; index < eRadioArreglo.length; index++) {
+        const elemento = eRadioArreglo[index];
+        const radioElegido = eRadioArreglo[index].checked;
+        if (radioElegido === true) {
+            var vRadio = elemento.value;
+        } else {
+            console.log("No está chequeada")
+        }
+    }
+
+    let vNombre = eNombre.value;
+    let vApellido = eApellido.value;
+    let vTelefono = eTelefono.value;
+    let vMotivo = eMotivo.value;
+    let vInstagram = eInstagram.value;
+    let vPaginaweb = ePaginaweb.value;
+    let vFacebook = eFacebook.value;
+
+    validarVacio("nombre")
+    validarVacio("apellido")
+    validarVacio("telefono")
+
+    let objeto = {
+        nombre:vNombre,
+        apellido:vApellido,
+        telefono:vTelefono,
+        motivo:vMotivo,
+        importancia:vRadio,
+        instagram:vInstagram,
+        paginaweb:vPaginaweb,
+        facebook:vFacebook
+    }
+
+
+
+    registrarContacto(objeto).then(()=> {
+        alert("Contacto registrado correctamente.")
+    }).catch((e) => {
+        console.log(e)
+    })
 }
-
 
 
 
@@ -41,8 +86,10 @@ function fuente() {
     let eBotones = document.getElementsByTagName("button")
     let eInput = document.getElementsByTagName("input");
     let eMotivo = document.getElementById("motivo")
+    let eMensaje = document.getElementById("mensaje")
     console.log(eFuente.style.fontSize)
-    console.log(eInput);
+    console.log(typeof(eInput));
+
     if (eFuente.style.fontSize != "30px"){
         // Bucle "for in" que permite acceder a los índices del objeto, en este caso llamado "eInput" de tipo <HTMLCollection>
         for (const e in eInput) {
@@ -59,6 +106,7 @@ function fuente() {
         }
         eFuente.style.fontSize = "30px";
         eMotivo.style.fontSize = "30px";
+        eMensaje.style.fontSize = "30px";
     } else {
         // Bucle "for in" que permite acceder a los índices del objeto, en este caso llamado "eInput" de tipo <HTMLCollection>
         for (const e in eInput) {
@@ -77,8 +125,23 @@ function fuente() {
         }
         eFuente.style.fontSize = "15px";
         eMotivo.style.fontSize = "15px";
+        eMensaje.style.fontSize = "15px"
     }
     
     return eInput
 }
     
+
+function validarVacio(idCampo) {
+    let elemento = document.getElementById(idCampo);
+    let valor = elemento.value;
+    let valorSinEspacios = valor.trim();
+    let eParrafoValor = document.getElementById("p"+idCampo);
+    if (valorSinEspacios != "") {
+        elemento.style.backgroundColor = "#85b789"
+        eParrafoValor.style.display = "inline"
+    } else {
+        elemento.style.backgroundColor = "#c21e56"
+        eParrafoValor.style.display = "none"
+    }
+}
